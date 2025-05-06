4f4bf7f3c43a403b66c3be7b4e4069a3bd722d4b
Freyster <126128421+Freys1er@users.noreply.github.com>
2025-05-06 06:52:15 -0500

    Styles

--- script.js
+++ script.js
@@ -176,7 +176,6 @@ async function sendCommandToAppsScript(command, isJsonRequest = false) {
 
 /** Handles Google Sign-In response. */
 function handleCredentialResponse(response) {
-    loginPrompt.querySelector('.login-message').textContent = 'Authenticating...';
     idToken = response.credential;
     console.log("Google Sign-In successful.");
     addOutputLine("<p style='color: #50fa7b;'>Authentication successful.</p>");
